ba2798091c5fe5f8ece4e8527ad608cceac8b1e3
Pavel Usov <usov.p@mail.ru>
2020-07-25 18:01:40 +1000

    top button add

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ document.querySelector(".burger").onclick = function() {
 	this.classList.toggle("burger-active");
 	document.querySelector("nav").classList.toggle("active");
 }
+
+$(function() {
+  // при нажатии на кнопку top_up
+  $('.top_up').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop:0
+    },1000);
+  })
+})
+// при прокрутке окна (window)
+$(window).scroll(function() {
+  // если пользователь прокрутил страницу более чем на 200px
+  if ($(this).scrollTop()>700) {
+    // то сделать кнопку top_up видимой
+    $('.top_up').fadeIn();
+  }
+  // иначе скрыть кнопку top_up
+  else {
+    $('.top_up').fadeOut();
+  }
+});
